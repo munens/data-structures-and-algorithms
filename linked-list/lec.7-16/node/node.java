77e516470818a0c7a3160de7c334dc74b35265b4
Munene Kaumbutho <kaumbuthom@yahoo.com>
2020-06-04 17:53:04 -0700

    section 4: lec.7 - 9

---
+++ linked-list/lec.7-16/node/node.java
@@ -0,0 +1,27 @@
+package linkedList;
+
+class Node {
+
+  private int value;
+  private Node next;
+
+  public Node() {
+
+  }
+
+  public int getValue() {
+    return this.value;
+  }
+
+  public void setValue(int value) {
+    this.value = value;
+  }
+
+  public Node getNext() {
+    return this.node
+  }
+
+  public void setNext(Node next) {
+    this.next = next;
+  }
+}
